a42923e2abc79e009dc59a5448f281df1b2f219a
Guilherme Rito <guilhermemtr@gmail.com>
2017-08-14 22:47:15 +0100

    Updates

--- Word.py
+++ Word.py
@@ -7,6 +7,29 @@ from nltk.corpus import wordnet as wn
 def getLineString(line, prefix = "", suffix = ""):
     return prefix + line.capitalize() + suffix + getSuffix()
 
+def adj():
+    return "adjective"
+
+def adjSat():
+    return "satellite adjective"
+
+def adv():
+    return "adverb"
+
+def noun():
+    return "noun"
+
+def verb():
+    return "verb"
+
+wordType = {
+            'a' : adj,
+            's' : adjSat,
+            'r' : adv,
+            'n' : noun,
+            'v' : verb,
+}
+
 class ContextWord:
     def __init__(self, meaning):
         self.meaning = meaning
@@ -14,6 +37,9 @@ class ContextWord:
     def word(self):
         return self.meaning.lemmas()[0].name()
 
+    def pos(self):
+        return wordType[self.meaning.pos()]()
+        
     def getWordDefinition(self, prefix):
         return getLineString('Definition', prefix, ":") + getLineString(self.meaning.definition(), prefix, ".")
 
@@ -25,21 +51,26 @@ class ContextWord:
             examples += getLineString("None", prefix, ".")
         return examples
 
-    def getWordLemmas(self):
-        return meaning.lemmas()
-
-    def getWordSynonymLemmas(self):
-        return self.meaning.lemmas()
-        
     def getWordSynonyms(self, prefix):
         synonyms = getLineString('Synonyms', prefix, ":")
         for synonym in self.meaning.lemmas():
             syn = synonym.name()
             if syn != self.word():
-                synonyms += getLineString(syn, prefix + getPrefix(), ".")
+                synonyms += getLineString(syn.replace('_', ' '), prefix + getPrefix(), ".")
         if len(self.meaning.lemmas()) == 1:
             synonyms += getLineString("None", prefix, ".")
         return synonyms
+
+    def getWordAntonyms(self, prefix):
+        antonyms = getLineString('Antonyms', prefix, ":")
+        antonymCount = 0
+        for synonym in self.meaning.lemmas():
+            for antonym in synonym.antonyms():
+                antonyms += getLineString(antonym.name().replace('_', ' '), prefix + getPrefix(), ".")
+                antonymCount = antonymCount + 1
+        if antonymCount == 0:
+            antonyms += getLineString("None", prefix, ".")
+        return antonyms
 
     
     def getWordDescription(self, prefix, options):
@@ -53,6 +84,10 @@ class ContextWord:
 
         if options.getOptions()['synonyms']:
             description += self.getWordSynonyms(descPrefix)
+
+        if options.getOptions()['antonyms']:
+            description += self.getWordAntonyms(descPrefix)
+
         return description
 
 class Word:
@@ -73,7 +108,7 @@ class Word:
         for ctxWord in self.words:
             if ctxWordCount > 1:
                 description += getMajorPrefix(dictionaryRepresentationSpacingDefinitions["ctx-word"])
-            description += getLineString("Sense %d, as in %s" % (ctxWordCount,ctxWord.word()), pref, ":")
+            description += getLineString("Sense %d, as in %s (%s)" % (ctxWordCount,ctxWord.word(), ctxWord.pos()), pref, ":")
             description += ctxWord.getWordDescription(pref, options)
             ctxWordCount = ctxWordCount + 1
         return description
